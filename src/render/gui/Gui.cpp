cf5662eb0799c8cd7b899f87016445ce45777370
Primemaster <cesarsky5@gmail.com>
2019-07-30 12:56:18 +0100

    GUI rendering

--- src/render/gui/Gui.cpp
+++ src/render/gui/Gui.cpp
@@ -1,9 +1,12 @@
+#include <utility>
 #include "Gui.h"
 
 GLFWwindow* Gui::window = nullptr;
 
 Gui::Gui(GLFWwindow* window)
 {
+	window_hash.rehash(30);
+
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 
@@ -33,18 +36,60 @@ void Gui::newFrame() const
 
 void Gui::render() const
 {
-	bool l = true;
-	float f;
+	static bool l = true;
+	static float f = 0.0f;
+	static double lt = glfwGetTime();
+	static bool p_open = true;
 
-	ImGui::Begin("Hello, world!");
+	int k0 = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT);
+	int k1 = glfwGetKey(window, GLFW_KEY_F3);
 
-	ImGui::Text("This is some useful text.");
-	ImGui::Checkbox("Demo Window", &l);
-	ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-	ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+	if (k0 == GLFW_PRESS && k1 == GLFW_PRESS && glfwGetTime() - lt > 1)
+	{
+		p_open = !p_open;
+		lt = glfwGetTime();
+	}
 
-	ImGui::End();
+	if (p_open)
+	{
+		const float DISTANCE = 10.0f;
+		static int corner = 0;
+
+		ImGuiIO& io = ImGui::GetIO();
+		ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
+		ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
+		ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+		ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+
+	
+		if (ImGui::Begin("A", &p_open, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav))
+		{
+			ImGui::Text("    [Statistics]");
+			ImGui::Separator();
+			ImGui::TextColored(ImColor(1.0f, 1.0f, 0.0f), "Framerate: %9.4f\n", io.Framerate);
+			ImGui::TextColored(ImColor(1.0f, 0.0f, 1.0f), "Frametime: %9.4f\n", 1000.0f / io.Framerate);
+		}
+		ImGui::End();
+	}
+
+	for (auto window : window_hash)
+	{
+		window.second.first(window.second.second);
+	}
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void Gui::pushWindow(std::function<bool*> body)
+{
+	static unsigned window_push_ct = 0;
+	std::pair<std::function<bool*>, bool*> pair = std::make_pair(body, new bool(true));
+	window_hash.emplace(window_push_ct++, pair);
+}
+
+void Gui::popWindow(unsigned id)
+{
+	delete window_hash.at(id).second;
+	window_hash.erase(id);
 }
--- src/render/gui/Gui.h
+++ src/render/gui/Gui.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <iostream>
+#include <functional>
+#include <unordered_map>
+
+#include <GLFW/glfw3.h>
 #include "../../../deps/imgui/imgui.h"
 #include "../../../deps/imgui/imgui_impl_glfw.h"
 #include "../../../deps/imgui/imgui_impl_opengl3.h"
@@ -24,6 +29,10 @@ public:
 	void newFrame() const;
 	void render() const;
 
+	void pushWindow(std::function<bool*> body);
+	void popWindow(unsigned id);
+
 private:
 	static GLFWwindow* window;
+	std::unordered_map<unsigned, std::pair<std::function<void(bool*)>, bool*>> window_hash;
 };
